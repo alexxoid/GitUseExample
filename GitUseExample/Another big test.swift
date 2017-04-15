88bac79045c33ce005ec90083505dd96dc5407fc
Алексей Пирогов <alexxoid@alexxoid-mbp.local>
2017-04-15 12:20:16 +0500

    New class from NSObject is from "Coca Touch Class"

---
+++ GitUseExample/Another big test.swift
@@ -0,0 +1,13 @@
+//
+//  Another big test.swift
+//  GitUseExample
+//
+//  Created by Алексей Пирогов on 15.04.17.
+//  Copyright © 2017 Алексей Пирогов. All rights reserved.
+//
+
+import UIKit
+
+class Another_big_test: NSObject {
+
+}
